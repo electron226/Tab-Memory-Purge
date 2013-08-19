a0346ab5c637691f0d8becb64863318f27235ff9
electron226 <electron226@gmail.com>
2013-08-20 07:04:10 +0900

    modify test code.

--- spec/javascripts/commonSpec.js
+++ spec/javascripts/commonSpec.js
@@ -1,27 +1,30 @@
+/*jshint globalstrict: true*/
+"use strict";
+
 describe('Common Function', function () {
     it("typeofやinstanceofを統合", function() {
-        expect( getType({}) ).toEqual('object');
-        expect( getType(undefined) ).toEqual('undefined');
-        expect( getType(null) ).toEqual('null');
-        expect( getType(function(){}) ).toEqual('function');
-        expect( getType([]) ).toEqual('array');
-        expect( getType('string') ).toEqual('string');
-        expect( getType(1) ).toEqual('number');
-        expect( getType(true) ).toEqual('boolean');
+        expect( toType({}) ).toEqual('object');
+        expect( toType(undefined) ).toEqual('undefined');
+        expect( toType(null) ).toEqual('null');
+        expect( toType(function(){}) ).toEqual('function');
+        expect( toType([]) ).toEqual('array');
+        expect( toType('string') ).toEqual('string');
+        expect( toType(1) ).toEqual('number');
+        expect( toType(true) ).toEqual('boolean');
     });
 
     it("重複要素削除", function() {
-        expect( function() { unique([]) } ).not.toThrow();
-        expect( function() { unique(1) } ).toThrow();
-        expect( function() { unique({}) } ).toThrow();
+        expect( function() { unique([]); } ).not.toThrow();
+        expect( function() { unique(1); } ).toThrow();
+        expect( function() { unique({}); } ).toThrow();
         expect( unique(["array", "bebe", "array", "bebe"]) ).toEqual(["array",
                                                                       "bebe"]);
     });
 
     it("trim", function() {
-        expect(function() { trim("") }).not.toThrow();
-        expect(function() { trim(1) }).toThrow();
-        expect(function() { trim(new Object()) }).toThrow();
+        expect(function() { trim(""); }).not.toThrow();
+        expect(function() { trim(1); }).toThrow();
+        expect(function() { trim({}); }).toThrow();
         expect( trim(" hello  ") ).toEqual("hello");
     });
 
@@ -31,12 +34,5 @@ describe('Common Function', function () {
         var z = [ 1, 2, 3, 4 ];
         expect(arrayEqual(i, j)).toBeTruthy();
         expect(arrayEqual(i, z)).toBeFalsy();
-    });
-
-    it('sleep', function() {
-        /* var d1 = new Date().getTime();
-        sleep(1);
-        var d2 = new Date().getTime();
-        expect(d2 - d1).toBeLessThan(1.1); */
     });
 });
