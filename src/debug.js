de887282a5ea3538ad40085a5789b767a84ce3ed
electron226 <electron226@gmail.com>
2014-10-26 09:28:18 +0900

    debug code is deleted.

--- src/debug.js
+++ src/debug.js
@@ -2,11 +2,11 @@
 "use strict";
 
 // console log settings.
-// console.log = function() {};
-// console.warn = function() {};
-// console.debug = function() {};
-// console.assert = function() {};
-// console.time = function() {};
-// console.timeEnd = function() {};
-// console.timeStamp = function() {};
+console.log = function() {};
+console.warn = function() {};
+console.debug = function() {};
+console.assert = function() {};
+console.time = function() {};
+console.timeend = function() {};
+console.timestamp = function() {};
 
