14e9f684d9337e5a454a068562d34d0bf6df7b0f
electron <electron226@gmail.com>
2015-09-30 17:07:37 +0900

    Modify: document.scrollingElement also use when get the scroll value of a page.

--- src/js/load_scripts/getScrollPosition.js
+++ src/js/load_scripts/getScrollPosition.js
@@ -1,7 +1,11 @@
 /** ページのスクロール量を取得するのに使用する */
 var rObj = {};
-rObj.x = document.documentElement.scrollLeft || document.body.scrollLeft;
-rObj.y = document.documentElement.scrollTop || document.body.scrollTop;
+rObj.x = document.scrollingElement.scrollLeft ||
+         document.documentElement.scrollLeft ||
+         document.body.scrollLeft;
+rObj.y = document.scrollingElement.scrollLeft ||
+         document.documentElement.scrollTop ||
+         document.body.scrollTop;
 // jshint ignore:start
 rObj; // executeScriptのコールバック関数に返る値
 // jshint ignore:end
