5a48b98a4fd6d7c5698877a60b7f43ce8339b844
electron <electron226@gmail.com>
2015-10-01 23:09:33 +0900

    Fixed: the release build doesn't load the scroll position of a purge tab correctly

--- src/js/load_scripts/getScrollPosition.js
+++ src/js/load_scripts/getScrollPosition.js
@@ -1,11 +1,16 @@
 /** ページのスクロール量を取得するのに使用する */
-var rObj = {};
-rObj.x = document.scrollingElement.scrollLeft ||
-         document.documentElement.scrollLeft ||
-         document.body.scrollLeft;
-rObj.y = document.scrollingElement.scrollLeft ||
-         document.documentElement.scrollTop ||
-         document.body.scrollTop;
-// jshint ignore:start
-rObj; // executeScriptのコールバック関数に返る値
-// jshint ignore:end
+function getScrollPosition()
+{
+  'use strict';
+
+  var rObj = {};
+  rObj.x = document.scrollingElement.scrollLeft ||
+           document.documentElement.scrollLeft ||
+           document.body.scrollLeft;
+  rObj.y = document.scrollingElement.scrollLeft ||
+           document.documentElement.scrollTop ||
+           document.body.scrollTop;
+  return rObj; // executeScriptのコールバック関数に返る値
+}
+
+getScrollPosition();
